ca349058b0e1bb16526cc65f969086117f87f126
Tom Malhi <tommalhi1@gmail.com>
2023-10-24 10:38:17 +0300

    Added Login and sign up pages

---
+++ app/src/main/java/com/example/movieroullette/LandingPage.java
@@ -0,0 +1,33 @@
+package com.example.movieroullette;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LandingPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.landing_page);
+        
+        // login button
+        findViewById(R.id.login_button).setOnClickListener(
+                view -> {
+                    Intent intent = new Intent(getApplicationContext(), LoginPage.class);
+                    startActivity(intent);
+                }
+        );
+        // sign up button
+        findViewById(R.id.sign_up_button).setOnClickListener(
+                view -> {
+                    Intent intent = new Intent(getApplicationContext(), SignUpPage.class);
+                    startActivity(intent);
+                }
+        );
+        
+    }
+}
