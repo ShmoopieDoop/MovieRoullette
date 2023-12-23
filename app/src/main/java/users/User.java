d4180cb05366bc238245b26a9f545ef1be2c7209
Tom Malhi <tommalhi1@gmail.com>
2023-12-23 22:43:10 +0200

    Partially working firebase auth

--- app/src/main/java/users/User.java
+++ app/src/main/java/users/User.java
@@ -1,21 +1,42 @@
 package users;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 public class User {
-    final public String name;
+    private final String name;
     private final String email;
     private final UUID uuid;
+    private final Map<UUID, Boolean> groups;
 
-    public User(String name, String email) {
+    public User(String name, String email, Map<UUID, Boolean> groups) {
         this.name = name;
         this.email = email;
         this.uuid = UUID.randomUUID();
+        this.groups = groups;
     }
 
-    public User(String name, String email, UUID uuid) {
+    public User(String name, String email, UUID uuid, Map<UUID, Boolean> groups) {
         this.name = name;
         this.email = email;
         this.uuid = uuid;
+        this.groups = groups;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public Map<UUID, Boolean> getGroups() {
+        return groups;
     }
 }
